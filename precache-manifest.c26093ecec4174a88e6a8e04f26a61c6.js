20064172dddbbddd33bbed5d1c7feed2970e5957
Amir <dardar4@gmail.com>
2020-09-16 22:25:19 +0300

    Updates

---
+++ precache-manifest.c26093ecec4174a88e6a8e04f26a61c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dc26cc3414a89853d29e5b097462f1",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "f73e88f2a99b70d66e6e",
+    "url": "/Weather-App/static/css/main.a11368d0.chunk.css"
+  },
+  {
+    "revision": "ff513d6a1b548b1fac27",
+    "url": "/Weather-App/static/js/2.6afc1027.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Weather-App/static/js/2.6afc1027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f73e88f2a99b70d66e6e",
+    "url": "/Weather-App/static/js/main.55a2c458.chunk.js"
+  },
+  {
+    "revision": "5a89754edee3862e9a0e",
+    "url": "/Weather-App/static/js/runtime-main.49cbaed4.js"
+  }
+]);
